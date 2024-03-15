addadfb847178999bac93f9873045fc0a6173301
yun <yunjinqi@qq.com>
2024-03-15 23:11:26 +0800

    update compile script

--- compile_cython_numba_files.py
+++ compile_cython_numba_files.py
@@ -1,7 +1,16 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
+import sys
+
 for root, dirs, files in os.walk('./utils/'):
+    if sys.platform == 'win32':
+        pass
+    elif sys.platform == 'linux':
+        if "my_corr" in root:
+            continue
+    elif sys.platform == 'darwin':
+        pass
     compile_cython = False
     compile_numba = False
     for file in files:
