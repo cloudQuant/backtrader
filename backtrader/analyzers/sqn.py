f2986f00195ffa141e65a0dfd60671a8ad6bdbd7
yunjinqi <yunjinqi@qq.com>
2025-10-26 09:52:52 +0800

    update remain 3

--- backtrader/analyzers/sqn.py
+++ backtrader/analyzers/sqn.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
+
 import math
 from backtrader import Analyzer
 from backtrader.mathsupport import average, standarddev
 from backtrader.utils import AutoOrderedDict
+
+
+__all__ = ['SQN']
 
 
 # 获取SQN指标
@@ -16,7 +20,7 @@ class SQN(Analyzer):
       - 2.5 - 2.9 Good
       - 3.0 - 5.0 Excellent
       - 5.1 - 6.9 Superb
-      - 7.0 - Holy Grail?
+      - 7.0 -     Holy Grail?
 
     The formula:
 
@@ -26,67 +30,44 @@ class SQN(Analyzer):
 
     Methods:
 
-      - Get_analysis
+      - get_analysis
 
         Returns a dictionary with keys "sqn" and "trades" (number of
         considered trades)
 
     """
-
     # 系统质量数
-    alias = ("SystemQualityNumber",)
+    alias = ('SystemQualityNumber',)
 
     # 创建分析
     def create_analysis(self):
         """Replace default implementation to instantiate an AutoOrderedDict
         rather than an OrderedDict"""
         self.rets = AutoOrderedDict()
-        self.rets.sqn = 0.0
-        self.rets.trades = 0
 
     # 开始，初始化pnl和count
     def start(self):
         super(SQN, self).start()
-        self._trades = []
+        self.pnl = list()
+        self.count = 0
 
     # 交易通知，如果trade是关闭的，添加盈亏
     def notify_trade(self, trade):
-        if trade.isclosed:
-            # Get profit/loss for this trade
-            pnl = trade.pnl
-            
-            # Create trades list if not exists
-            if not hasattr(self, '_trades'):
-                self._trades = []
-            
-            # Add trade pnl to list
-            self._trades.append(pnl)
-            
-            # Update trade count
-            self.rets.trades = len(self._trades)
-            
-            # Calculate SQN if we have enough trades
-            if len(self._trades) > 1:
-                avg_pnl = average(self._trades)
-                std_pnl = standarddev(self._trades)
-                
-                if std_pnl > 0.0:
-                    sqn = math.sqrt(len(self._trades)) * avg_pnl / std_pnl
-                    self.rets.sqn = sqn
-                else:
-                    self.rets.sqn = 0.0
-            else:
-                self.rets.sqn = 0.0
+        if trade.status == trade.Closed:
+            self.pnl.append(trade.pnlcomm)
+            self.count += 1
 
     # 停止，计算sqn指标，如果交易次数大于0，sqn等于交易盈利的平均值*交易次数的平方根/交易盈利的标准差
     def stop(self):
-        # Final calculation in case needed
-        if hasattr(self, '_trades') and len(self._trades) > 1:
-            avg_pnl = average(self._trades)
-            std_pnl = standarddev(self._trades)
-            
-            if std_pnl > 0.0:
-                sqn = math.sqrt(len(self._trades)) * avg_pnl / std_pnl
-                self.rets.sqn = sqn
-            else:
-                self.rets.sqn = 0.0
+        if self.count > 1:
+            pnl_av = average(self.pnl)
+            pnl_stddev = standarddev(self.pnl)
+            try:
+                sqn = math.sqrt(len(self.pnl)) * pnl_av / pnl_stddev
+            except ZeroDivisionError:
+                sqn = None
+        else:
+            sqn = 0
+        # 设置sqn的值和trades的值
+        self.rets.sqn = sqn
+        self.rets.trades = self.count
