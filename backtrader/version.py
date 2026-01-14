6cc261442e661cb9f1993213e0071942fd0d7292
cloudQuant <yunjinqi@gmail.com>
2026-01-14 21:55:59 +0800

    chore: release v1.0.1

--- backtrader/version.py
+++ backtrader/version.py
@@ -15,7 +15,7 @@ Example:
     1.9.76.123
 """
 # backtrader version number
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # backtrader version number, tuple format
 __btversion__ = tuple(int(x) for x in __version__.split("."))
